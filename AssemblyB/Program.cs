6659a0ef3bbe37bc4bc7579f4253940f5998cf23
ahmed jardak <ahmed.jardak@hotmail.com>
2015-03-30 15:00:30 +0200

    refactor Code

--- AssemblyB/Program.cs
+++ AssemblyB/Program.cs
@@ -4,7 +4,7 @@ namespace AssemblyB
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             new ClassWithFriendMethod().Test();
             new FriendClass().Test();
