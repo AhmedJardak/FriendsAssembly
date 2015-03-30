3d364237ab749121a080f9a4a16d64056349c7cf
ahmed jardak <ahmed.jardak@hotmail.com>
2015-03-30 13:00:37 +0200

    Friends Assembly

---
+++ AssemblyA/FriendClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("AssemblyB")]
+namespace AssemblyA
+{
+    // The class is internal by default. 
+    internal class FriendClass
+    {
+        public void Test()
+        {
+            Console.WriteLine("Sample Class");
+        }
+    }
+
+    // Public class that has an internal method. 
+    public class ClassWithFriendMethod
+    {
+        internal void Test()
+        {
+            Console.WriteLine("Sample Method");
+        }
+
+    }
+}
---
+++ AssemblyB/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AssemblyA;
+
+namespace AssemblyB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new ClassWithFriendMethod().Test();
+            new FriendClass().Test();
+        }
+    }
+}
